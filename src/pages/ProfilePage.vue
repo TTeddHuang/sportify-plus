c980517f91d0816e9d936074be9e1f29c9aa8275
HWS-Ted <f772886@gmail.com>
2025-05-05 17:35:55 +0800

    feat: complete user-profile layout slicing

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,63 +1,96 @@
 <template>
-  <div class="container mt-5">
-    <h2>個人資料</h2>
-    <div v-if="user">
-      <div class="mb-3">
-        <label class="form-label">Email</label>
-        <input
-          v-model="user.email"
-          type="email"
-          class="form-control"
-          disabled
-        />
+  <div class="learning-center row container">
+    <div class="side-nav col-3">
+      <div class="px-3 py-5">
+        <h3 class="fs-6 px-3 fw-bold">學習中心</h3>
+        <hr class="divider my-5" />
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">我的課程</li>
+          <li class="list-group-item">訂閱紀錄</li>
+          <li class="list-group-item active">編輯個人資料</li>
+        </ul>
       </div>
-      <div class="mb-3">
-        <label class="form-label">密碼</label>
-        <input v-model="password" type="password" class="form-control" />
-      </div>
-      <button class="btn btn-primary" @click="handleUpdate">更新資料</button>
     </div>
-    <div v-else>
-      <p>載入中...</p>
+    <div class="user-profile col-9">
+      <h2 class="fs-4">個人資料</h2>
+      <div class="profile-group row">
+        <div class="col-6">
+          <div
+            class="profile-avatar ratio ratio-1x1 rounded-circle overflow-hidden mx-auto"
+          >
+            <img
+              src="https://ankemedia.com/wp-content/uploads/2021/12/%E6%88%91%E6%98%AF%E9%81%BA%E7%85%A7%E6%94%9D%E5%BD%B1%E5%B8%AB1-1.jpg"
+              alt="個人照片"
+              class="object-fit-cover"
+            />
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="mb-3">
+            <label class="form-label">會員名</label>
+            <input type="text" class="form-control" />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">Email</label>
+            <input type="email" class="form-control" />
+          </div>
+          <div class="mb-3">
+            <label class="form-label">密碼</label>
+            <input type="password" class="form-control" />
+          </div>
+          <button class="btn btn-primary-600 mt-1 d-block mx-auto">
+            編輯個人資料
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-import { getUserProfile, updateUserProfile } from '@/api/auth'
-import { useRouter } from 'vue-router'
+<style scoped lang="scss">
+.container {
+  max-width: 1296px;
+  margin: 0 auto;
+}
 
-const user = ref(null)
-const password = ref('')
-const router = useRouter()
+.side-nav,
+.user-profile {
+  border: 1px solid $primary-100;
+}
 
-// 驗證使用者是否登入
-onMounted(async () => {
-  const token = localStorage.getItem('token')
-  if (!token) {
-    // 如果沒有 token，跳轉到登入頁面
-    router.push('/login')
-    return
+.side-nav {
+  padding: 0;
+}
+
+.divider {
+  opacity: 1;
+  border: 0;
+  border-top: 1px solid $primary-000;
+}
+
+.list-group {
+  .list-group-item {
+    border: none;
+    padding: 12px 16px;
+    margin-bottom: 8px;
   }
-
-  try {
-    const res = await getUserProfile()
-    user.value = res.data
-  } catch (error) {
-    console.error(error)
-    router.push('/login')
-  }
-})
-
-async function handleUpdate() {
-  try {
-    const updatedUser = await updateUserProfile({ password: password.value })
-    alert('資料更新成功')
-    user.value = updatedUser.data
-  } catch (error) {
-    console.error(error)
-    alert(error.response?.data?.message || '資料更新失敗')
+  .active {
+    border-radius: 4px;
+    color: $primary-100;
   }
 }
-</script>
+
+.user-profile {
+  padding: 40px;
+  .profile-group {
+    margin-top: 40px;
+  }
+  .profile-avatar {
+    width: 300px;
+  }
+}
+
+.btn-primary {
+  padding: 8px 16px;
+}
+</style>
