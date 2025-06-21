302c288436474a0ddbe9ab04bf7f6932331988a4
HWS-Ted <f772886@gmail.com>
2025-06-22 01:00:56 +0800

    fix. 修正footer導向、補全教練個人頁面的錯誤提示

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -5,33 +5,33 @@
     <div class="mb-6">
       <div class="container text-center">
         <!-- Logo -->
-        <router-link to="/">
+        <a>
           <img
             src="@/assets/images/logo-s.png"
             alt="Logo"
             class="mb-4 mb-7"
             style="height: 50px"
           />
-        </router-link>
+        </a>
         <!-- 導覽連結 -->
         <div class="d-flex justify-content-center gap-4 mb-3 flex-wrap">
-          <router-link to="/about" class="px-1 py-2">關於我們</router-link>
-          <router-link to="/contact" class="px-1 py-2">聯絡我們</router-link>
-          <router-link to="/privacy" class="px-1 py-2">隱私權政策</router-link>
-          <router-link to="/terms" class="px-1 py-2">使用者條款</router-link>
+          <a class="px-1 py-2">關於我們</a>
+          <a class="px-1 py-2">聯絡我們</a>
+          <a class="px-1 py-2">隱私權政策</a>
+          <a class="px-1 py-2">使用者條款</a>
         </div>
       </div>
     </div>
 
     <div>
       <div class="d-flex justify-content-center gap-4 mb-5">
-        <a href="#" class="text-primary-000" style="font-size: 32px"
+        <a class="text-primary-000" style="font-size: 32px"
           ><i class="bi bi-facebook"></i
         ></a>
-        <a href="#" class="text-primary-000" style="font-size: 32px"
+        <a class="text-primary-000" style="font-size: 32px"
           ><i class="bi bi-instagram"></i
         ></a>
-        <a href="#" class="text-primary-000" style="font-size: 32px"
+        <a class="text-primary-000" style="font-size: 32px"
           ><i class="bi bi-line"></i
         ></a>
       </div>
